2885410f41b9c9af11f1e240f66096b445eba597
Animesh Kant <akant.30@gmail.com>
2023-05-04 02:00:51 +0530

    pagination

--- src/app/ticket/tickets-list/tickets-list.component.ts
+++ src/app/ticket/tickets-list/tickets-list.component.ts
@@ -1,17 +1,23 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
-import {MatTableDataSource} from '@angular/material/table';
+import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
 import { Tickets } from '../tickets';
 
 @Component({
   selector: 'app-tickets-list',
   templateUrl: './tickets-list.component.html',
-  styleUrls: ['./tickets-list.component.scss']
+  styleUrls: ['./tickets-list.component.scss'],
 })
 export class TicketsListComponent implements OnInit, AfterViewInit {
-  displayedColumns: string[] = ['ticketName', 'displayId', 'dateOfCreation', 'description', 'status'];
+  displayedColumns: string[] = [
+    'ticketName',
+    'displayId',
+    'dateOfCreation',
+    'description',
+    'status',
+  ];
   dataSource!: MatTableDataSource<Tickets>;
   statusOptions = ['Open', 'In-progress', 'Complete', 'Deferred'];
   public statusDrop: any;
@@ -19,21 +25,18 @@ export class TicketsListComponent implements OnInit, AfterViewInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-
-  constructor(private router: Router,) {
-
-  }
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
     const localdata = localStorage.getItem('Tickets');
-    if(localdata != null) {
-      this.dataSource = JSON.parse(localdata);
+    if (localdata != null) {
+      this.dataSource = new MatTableDataSource<Tickets>(JSON.parse(localdata));
     }
-    
   }
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
   public create() {
